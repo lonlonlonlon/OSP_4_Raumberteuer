d66754142ef7e85029d157ef43e03385ce94384c
ext-lolb@d-velop.de <ext-lolb@d-velop.de>
2022-11-15 07:34:25 +0100

    Updated ./startProject.sh - username fix

--- getUsername.php
+++
@@ -1,7 +0,0 @@
-<?php
-$path = getcwd();
-$parts = explode('/', $path);
-if ($parts[2] == 'www') {
-    $parts[2] = 'www-data';
-}
-echo($parts[2].PHP_EOL);
--- startProject.sh
+++ startProject.sh
@@ -10,28 +10,25 @@ fi
 # stop php webserver
 pkill -e -f 'sudo php -S localhost:80 ./public/index.php'
 
-USERNAME="$(php ./getUsername.php)"
-USERNAME="software"
-
 # stop db
-sudo -u $USERNAME docker-compose down
+sudo -u $SUDO_USER docker-compose down
 
 # run docker for db
-sudo -u $USERNAME docker-compose up -d --force-recreate
+sudo -u $SUDO_USER docker-compose up -d --force-recreate
 
 # fix db ip in .env
-sudo -u $USERNAME php genEnv.php
+sudo -u $SUDO_USER php genEnv.php
 
 # run composer install
-sudo -u $USERNAME composer install
-sudo -u $USERNAME composer dump-autoload
+sudo -u $SUDO_USER composer install
+sudo -u $SUDO_USER composer dump-autoload
 
 # execute migrations
-sudo -u $USERNAME php bin/console doc:mig:mig --no-interaction
+sudo -u $SUDO_USER php bin/console doc:mig:mig --no-interaction
 
 # clear cache
-sudo -u $USERNAME php bin/console cache:clear
-sudo -u $USERNAME php bin/console cache:warmup
+sudo -u $SUDO_USER php bin/console cache:clear
+sudo -u $SUDO_USER php bin/console cache:warmup
 
 # run php build in webserver as demo webserver
 php -S localhost:8080 ./public/index.php
